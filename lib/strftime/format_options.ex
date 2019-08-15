e1fa4d77226e067c88a7deb588477c2af12ced5c
Gustavo Santos Ferreira <gustavo.ferreira@plataformatec.com.br>
2019-08-15 14:30:17 -0300

    add docs and typespecs for FormatOptions

--- lib/strftime/format_options.ex
+++ lib/strftime/format_options.ex
@@ -1,6 +1,21 @@
 defmodule Strftime.FormatOptions do
+  @moduledoc """
+  Module for setting and retrieving configurable formatting options
+  """
   alias Strftime.FormatOptions
 
+  @typedoc """
+  Struct that holds configuration options, loads the application defaults on the configs
+  """
+  @type t :: %__MODULE__{
+          preferred_date: String.t(),
+          preferred_time: String.t(),
+          preferred_datetime: String.t(),
+          am_pm_names: {String.t(), String.t()},
+          month_names: list(String.t()),
+          day_of_week_names: list(String.t()),
+          abbreviation_size: integer()
+        }
   defstruct preferred_date: Application.get_env(:strftime, :preferred_date, "%Y-%m-%d"),
             preferred_time: Application.get_env(:strftime, :preferred_time, "%H:%M:%S"),
             preferred_datetime:
@@ -20,7 +35,10 @@ defmodule Strftime.FormatOptions do
               ),
             abbreviation_size: 3
 
+  @typedoc "Accepted configuration options to be used by Strftime"
   @type options :: [option]
+
+  @typedoc "Accepted configuration option to be used by Strftime"
   @type option ::
           {:preferred_date, String.t()}
           | {:preferred_time, String.t()}
@@ -29,23 +47,38 @@ defmodule Strftime.FormatOptions do
           | {:month_names, list(String.t())}
           | {:day_of_week_names, list(String.t())}
 
-  def am_name(options = %FormatOptions{}), do: elem(options.am_pm_names, 0)
+  @doc "Returns the `am_name` configured for the received format_options struct"
+  @spec am_name(t()) :: String.t()
+  def am_name(format_options = %FormatOptions{}), do: elem(format_options.am_pm_names, 0)
 
-  def pm_name(options = %FormatOptions{}), do: elem(options.am_pm_names, 1)
+  @doc "Returns the `pm_name` configured for the received format_options struct"
+  @spec pm_name(t()) :: String.t()
+  def pm_name(format_options = %FormatOptions{}), do: elem(format_options.am_pm_names, 1)
 
-  def month_name(index, options = %FormatOptions{}) when index > 0 and index < 13 do
-    Enum.at(options.month_names, index - 1)
+  @doc "Returns the name of the received month number, based on the received format_options struct"
+  @spec month_name(integer(), t()) :: String.t()
+  def month_name(index, format_options = %FormatOptions{}) when index > 0 and index < 13 do
+    Enum.at(format_options.month_names, index - 1)
   end
 
-  def month_name_abbreviated(index, options = %FormatOptions{}) do
-    String.slice(month_name(index, options), 0..(options.abbreviation_size - 1))
+  @doc "Same as `month_name` but shortened to the configured `:abbreviation_size`"
+  @spec month_name_abbreviated(integer(), t()) :: String.t()
+  def month_name_abbreviated(index, format_options = %FormatOptions{}) do
+    String.slice(month_name(index, format_options), 0..(format_options.abbreviation_size - 1))
   end
 
-  def day_of_week_name(index, options = %FormatOptions{}) when index > 0 and index < 8 do
-    Enum.at(options.day_of_week_names, index - 1)
+  @doc "Returns the name of the received week day number, based on the received options struct"
+  @spec day_of_week_name(integer(), t()) :: String.t()
+  def day_of_week_name(index, format_options = %FormatOptions{}) when index > 0 and index < 8 do
+    Enum.at(format_options.day_of_week_names, index - 1)
   end
 
-  def day_of_week_name_abbreviated(index, options = %FormatOptions{}) do
-    String.slice(day_of_week_name(index, options), 0..(options.abbreviation_size - 1))
+  @doc "Same as `day_of_week_name` but shortened to the configured `:abbreviation_size`"
+  @spec day_of_week_name_abbreviated(integer(), t()) :: String.t()
+  def day_of_week_name_abbreviated(index, format_options = %FormatOptions{}) do
+    String.slice(
+      day_of_week_name(index, format_options),
+      0..(format_options.abbreviation_size - 1)
+    )
   end
 end
