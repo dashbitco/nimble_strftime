43d89f8b0c59de02ba51ad1e15c4431152919878
Gustavo Santos Ferreira <gustavo.ferreira@plataformatec.com.br>
2019-08-15 10:20:56 -0300

    redo the main module to use recursion instead of regex

--- lib/strftime.ex
+++ lib/strftime.ex
@@ -1,7 +1,9 @@
 defmodule Strftime do
   @moduledoc """
-  Documentation for Strftime.
+  A module that is meant to format a date into a string
   """
+  alias Strftime.FormatStream
+  alias Strftime.FormatOptions
 
   @empty_date %{
     day: 1,
@@ -17,75 +19,84 @@ defmodule Strftime do
     zone_abbr: ""
   }
 
-  @spec format(Date.t() | Time.t() | DateTime.t(), String.t()) :: String.t()
-  def format(date_or_time_or_datetime, string_format) do
-    Regex.scan(
-      ~r/(?<all_match>%(?<padding>(_|-|0)*)(?<length>\d*)(?<format>\w|%))/,
-      string_format,
-      capture: :all_names
+  @spec format(
+          Date.t() | Time.t() | NaiveDateTime.t() | DateTime.t(),
+          String.t(),
+          FormatOptions.options()
+        ) :: String.t()
+  def format(date_or_time_or_datetime, string_format, format_options \\ []) do
+    datetime =
+      @empty_date
+      |> Map.merge(date_or_time_or_datetime)
+
+    parse(string_format, datetime, Map.new(format_options))
+  end
+
+  defp parse(data, datetime, format_options, acc \\ "")
+  defp parse("", _datetime, _format_options, acc), do: acc
+
+  defp parse("%" <> rest, datetime, format_options, acc),
+    do: exec_stream(rest, datetime, acc, format_options)
+
+  defp parse(<<char::binary-1, rest::binary>>, datetime, format_options, acc) do
+    parse(rest, datetime, format_options, acc <> char)
+  end
+
+  defp exec_stream(data, datetime, acc, format_options) do
+    {format_stream, remaining} = FormatStream.stream(data, %FormatStream{})
+    options_struct = Map.merge(%FormatOptions{}, format_options)
+
+    parse(
+      remaining,
+      datetime,
+      format_options,
+      acc <> apply_stream(format_stream, datetime, options_struct)
     )
-    |> Enum.reduce(string_format, &format_chunk(date_or_time_or_datetime, &1, &2))
   end
 
-  defp format_chunk(date_or_time_or_datetime, [all_match, format, length, padding], string_format) do
-    padding_length = padding_length(length, format, padding)
-    padding_type = padding_type(padding, format)
+  defp apply_stream(format_stream, datetime, format_options \\ %FormatOptions{})
 
-    formatted_chunk =
-      case format do
-        # Literal “%” character
-        "%" ->
-          "%"
-
-        # format code
-        format ->
-          @empty_date
-          |> Map.merge(date_or_time_or_datetime)
-          |> format(format, padding_length, padding_type)
-      end
-
-    String.replace(string_format, all_match, formatted_chunk)
+  defp apply_stream(format_stream = %{format: format, pad: nil}, datetime, format_options) do
+    apply_stream(%{format_stream | pad: default_pad(format)}, datetime, format_options)
   end
 
-  defp padding_length(_length, _format, "-"), do: 0
+  defp apply_stream(format_stream = %{format: format, width: nil}, datetime, format_options) do
+    apply_stream(%{format_stream | width: default_width(format)}, datetime, format_options)
+  end
 
-  defp padding_length("", format, _padding) do
+  defp apply_stream(
+         format_stream = %FormatStream{format: format, width: width, pad: pad},
+         datetime,
+         format_options
+       ) do
     case format do
-      format when format in ~w(d H I m M S u y) -> 2
-      "j" -> 3
-      "Y" -> 4
-      _ -> 0
-    end
-  end
+      # Literal `%`
+      "%" ->
+        "%"
 
-  defp padding_length(length, _format, _padding), do: String.to_integer(length)
-
-  defp padding_type("0", _format), do: "0"
-  defp padding_type(padding, format) when padding == "_" or format in ~w(a A b B p P Z), do: " "
-  defp padding_type("", _format), do: "0"
-  defp padding_type("-", _format), do: "-"
-
-  defp format(datetime, format, padding_length, padding_type) do
-    case format do
       # Abbreviated name of day
       "a" ->
-        "TBD"
+        datetime
+        |> Date.day_of_week()
+        |> FormatOptions.day_of_week_name_abbreviated(format_options)
 
       # Full name of day
       "A" ->
-        "TBD"
+        datetime
+        |> Date.day_of_week()
+        |> FormatOptions.day_of_week_name(format_options)
 
       # Abbreviated month name
       "b" ->
-        "TBD"
+        FormatOptions.month_name_abbreviated(datetime.month(), format_options)
 
       # Full month name
       "B" ->
-        "TBD"
+        FormatOptions.month_name(datetime.month(), format_options)
 
       # Preferred date+time representation
       "c" ->
-        "TBD"
+        parse(format_options.preferred_datetime, datetime, format_options)
 
       # Day of the month
       "d" ->
@@ -105,7 +116,7 @@ defmodule Strftime do
 
       # Day of the year
       "j" ->
-        datetime.__struct__.day_of_year(datetime)
+        Date.day_of_year(datetime)
 
       # Month
       "m" ->
@@ -117,15 +128,19 @@ defmodule Strftime do
 
       # “AM” or “PM” (noon is “PM”, midnight as “AM”)
       "p" ->
-        if datetime.hour() < 11, do: "AM", else: "PM"
+        datetime.hour()
+        |> am_pm(format_options)
+        |> String.upcase()
 
       # “am” or “pm” (noon is “pm”, midnight as “am”)
       "P" ->
-        if datetime.hour() < 11, do: "am", else: "pm"
+        datetime.hour()
+        |> am_pm(format_options)
+        |> String.downcase()
 
       # Quarter
       "q" ->
-        datetime.__struct__.quarter_of_year(datetime)
+        Date.quarter_of_year(datetime)
 
       # Second
       "S" ->
@@ -133,21 +148,19 @@ defmodule Strftime do
 
       # Day of the week
       "u" ->
-        datetime.__struct__.day_of_week(datetime)
+        Date.day_of_week(datetime)
 
       # Preferred date (without time) representation
       "x" ->
-        "TBD"
+        parse(format_options.preferred_date, datetime, format_options)
 
       # Preferred time (without date) representation
       "X" ->
-        "TBD"
+        parse(format_options.preferred_time, datetime, format_options)
 
       # Year as 2-digits
       "y" ->
-        datetime.year()
-        |> Integer.to_string()
-        |> String.slice(-2..-1)
+        rem(datetime.year(), 100)
 
       # Year
       "Y" ->
@@ -163,8 +176,35 @@ defmodule Strftime do
       # Time zone abbreviation (empty string if naive)
       "Z" ->
         datetime.zone_abbr()
+
+      _ ->
+        format_stream.section <> format
     end
     |> to_string()
-    |> String.pad_leading(padding_length, padding_type)
+    |> String.pad_leading(width, pad)
+  end
+
+  defp am_pm(hour, format_options) when hour > 11 do
+    FormatOptions.pm_name(format_options)
+  end
+
+  defp am_pm(hour, format_options) when hour < 11 do
+    FormatOptions.am_name(format_options)
+  end
+
+  defp default_pad(format) do
+    case format do
+      format when format in ~w(a A b B p P Z) -> " "
+      _ -> "0"
+    end
+  end
+
+  defp default_width(format) do
+    case format do
+      format when format in ~w(d H I m M S u y) -> 2
+      "j" -> 3
+      "Y" -> 4
+      _ -> 0
+    end
   end
 end
