ac4b4aff48f99dd903070da7625834d27fa3cbae
Gustavo Santos Ferreira <gustavosantosfe@gmail.com>
2019-08-16 14:29:41 -0300

    fix offset representation

--- test/strftime_test.exs
+++ test/strftime_test.exs
@@ -15,6 +15,24 @@ defmodule StrftimeTest do
       assert Strftime.format(~T[17:07:57.001], "%-999M") == "7"
     end
 
+    test "format time zones correctly when receiving a DateTime" do
+      datetime_with_zone = %DateTime{
+        year: 2019,
+        month: 8,
+        day: 15,
+        zone_abbr: "EEST",
+        hour: 17,
+        minute: 7,
+        second: 57,
+        microsecond: {0, 0},
+        utc_offset: 7200,
+        std_offset: 3600,
+        time_zone: "UK"
+      }
+
+       assert Strftime.format(datetime_with_zone, "%z %Z") == "+0300 EEST"
+    end
+
     # this test won't pass due to problems in the current implementation
     # TODO: fix this
     @tag :skip
@@ -27,7 +45,7 @@ defmodule StrftimeTest do
                ~U[2019-08-15 17:07:57.001Z],
                "%04% %a %A %b %B %-3c %d %f %H %I %J %m %_5M %p %P %q %S %u %x %X %y %Y %z %Z"
              ) ==
-               "000% Thu Thursday Aug August 2019-08-15 17:07:57 15 1000 17 05 %J 08     7 PM pm 3 57 04 2019-08-15 17:07:57 19 2019 0, 0 UTC"
+               "000% Thu Thursday Aug August 2019-08-15 17:07:57 15 1000 17 05 %J 08     7 PM pm 3 57 04 2019-08-15 17:07:57 19 2019 +0000 UTC"
     end
 
     test "format according to received custom configs" do
